af7417790d95de1e052fbb036835bafe48fa3b8c
Libin Yang <libin.yang@intel.com>
2021-05-14 23:05:28 +0800

    unify the keys of the realtime output
    unify the keys of realtime outputsina, tencent and netease.
    
    Signed-off-by: Libin Yang <libin.yang@intel.com>

--- easyquotation/netease.py
+++ easyquotation/netease.py
@@ -1,6 +1,8 @@
 # coding:utf8
 import re
 import time
+import ast
+import datetime
 
 from . import basequotation, helpers
 
@@ -77,7 +79,42 @@ class netease(basequotation.BaseQuotation):
     def format_rt_response_data(self, rep_data, prefix=False, mode=helpers.MOD_RT):
         rep=re.findall(r'[(](.*)[)]', str(rep_data))
         rep="".join(rep)
-        return rep
+        tmpdict=ast.literal_eval(rep)
+        stock_dict = dict()
+        for key in tmpdict.keys():
+            stock_dict[key] = dict(
+                name=tmpdict[key]["name"],
+                open=tmpdict[key]["open"],
+                close=tmpdict[key]["yestclose"],
+                now=tmpdict[key]["price"],
+                high=tmpdict[key]["high"],
+                low=tmpdict[key]["low"],
+                turnover=tmpdict[key]["turnover"],
+                volume=tmpdict[key]["volume"],
+                bid1=tmpdict[key]["bid1"],
+                bid1_volume=tmpdict[key]["bidvol1"],
+                bid2=tmpdict[key]["bid2"],
+                bid2_volume=tmpdict[key]["bidvol2"],
+                bid3=tmpdict[key]["bid3"],
+                bid3_volume=tmpdict[key]["bidvol3"],
+                bid4=tmpdict[key]["bid4"],
+                bid4_volume=tmpdict[key]["bidvol4"],
+                bid5=tmpdict[key]["bid5"],
+                bid5_volume=tmpdict[key]["bidvol5"],
+                ask1=tmpdict[key]["ask1"],
+                ask1_volume=tmpdict[key]["askvol1"],
+                ask2=tmpdict[key]["ask2"],
+                ask2_volume=tmpdict[key]["askvol2"],
+                ask3=tmpdict[key]["ask3"],
+                ask3_volume=tmpdict[key]["askvol3"],
+                ask4=tmpdict[key]["ask4"],
+                ask4_volume=tmpdict[key]["askvol4"],
+                ask5=tmpdict[key]["ask5"],
+                ask5_volume=tmpdict[key]["askvol5"],
+                date=str(datetime.datetime.strptime(tmpdict[key]["update"], "%Y/%m/%d %H:%M:%S")).split()[0],
+                time=str(datetime.datetime.strptime(tmpdict[key]["update"], "%Y/%m/%d %H:%M:%S")).split()[1],
+                )
+        return stock_dict
 
     # history data format:
     # '日期,股票代码,名称,收盘价,最高价,最低价,开盘价,前收盘,涨跌额,涨跌幅,换手率,成交量,成交金额,总市值,流通市值,成交笔数'
@@ -147,12 +184,11 @@ class netease(basequotation.BaseQuotation):
             stock_item=re.findall(r'[^,]+', item)
             code=stock_item[1][-6:]
             stock_dict[stock_item[0]] = dict(
-                code=code, #股票代码,
                 name=stock_item[2], #名称
+                open=float(stock_item[6]), #开盘价
                 close=float(stock_item[3]), #收盘价
                 high=float(stock_item[4]), #最高价
                 low=float(stock_item[5]), #最低价
-                open=float(stock_item[6]), #开盘价
                 prev_close=float(stock_item[7]), #前收盘
                 change=float(stock_item[8]), #涨跌额
                 change_rate=float(stock_item[9]), #涨跌幅
